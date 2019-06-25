91b45ea8f9df90e199ba12b5c91bf9df65b10378
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-06-25 15:46:27 +0200

    Added currencies fragment

---
+++ app/src/main/java/com/wdowiak/financemanager/currencies/CurrenciesDisplayFragment.java
@@ -0,0 +1,65 @@
+package com.wdowiak.financemanager.currencies;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.wdowiak.financemanager.CommonDisplayFragment;
+import com.wdowiak.financemanager.accounts.AccountDetailActivity;
+import com.wdowiak.financemanager.data.Account;
+import com.wdowiak.financemanager.data.Currency;
+import com.wdowiak.financemanager.data.IItem;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class CurrenciesDisplayFragment extends CommonDisplayFragment<Currency, CurrenciesAdapter>
+{
+    @NotNull
+    @Contract(" -> new")
+    public static CurrenciesDisplayFragment newInstance()
+    {
+        return new CurrenciesDisplayFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState)
+    {
+        itemType = IItem.Type.Currency;
+
+        // todo, currencies detail does not exist yet
+        detailClass = AccountDetailActivity.class;
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(CurrenciesDisplayFragmentViewModel.class);
+
+        queryAndDisplayItems();
+    }
+
+    @Override
+    protected CurrenciesDisplayFragmentViewModel getViewModel()
+    {
+        return (CurrenciesDisplayFragmentViewModel) super.getViewModel();
+    }
+
+    @Override
+    protected CurrenciesAdapter createItemAdapter()
+    {
+        return new CurrenciesAdapter(viewModel.getData(), getActivity().getApplicationContext());
+    }
+}
---
+++ app/src/main/java/com/wdowiak/financemanager/currencies/CurrenciesDisplayFragmentViewModel.java
@@ -0,0 +1,10 @@
+package com.wdowiak.financemanager.currencies;
+
+import com.wdowiak.financemanager.DisplayFragmentViewModel;
+import com.wdowiak.financemanager.data.Currency;
+
+public class CurrenciesDisplayFragmentViewModel
+        extends DisplayFragmentViewModel<Currency, CurrenciesAdapter>
+{
+
+}
