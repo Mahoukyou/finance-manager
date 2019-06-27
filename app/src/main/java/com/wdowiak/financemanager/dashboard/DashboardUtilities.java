90152ee49254ea98309c3a98fb2ce7a975b29ab1
Dawid Wdowiak <dawidwdowiak@hotmail.com>
2019-06-27 19:28:54 +0200

    Some utilities, may be not used since I have changed the concept
    but keepein them for now

---
+++ app/src/main/java/com/wdowiak/financemanager/dashboard/DashboardUtilities.java
@@ -0,0 +1,83 @@
+package com.wdowiak.financemanager.dashboard;
+
+import com.wdowiak.financemanager.dashboard.category.CategoriesData;
+import com.wdowiak.financemanager.dashboard.category.CategoryData;
+import com.wdowiak.financemanager.data.Category;
+import com.wdowiak.financemanager.data.Transaction;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class DashboardUtilities
+{
+
+    public static CategoriesData createCategoriesData(@NotNull ArrayList<Transaction> transactions)
+    {
+        CategoriesData categoriesData = new CategoriesData();
+        categoriesData.categoriesHashMap = new HashMap<>();
+
+        for(Transaction transaction : transactions)
+        {
+            if(categoriesData.categoriesHashMap.containsKey(transaction.getCategory().getId()))
+            {
+                CategoryData categoryData = categoriesData.categoriesHashMap.get(transaction.getCategory().getId());
+                categoryData.amount += transaction.getAmount();
+                ++categoryData.count;
+            }
+            else
+            {
+                CategoryData categoryData = new CategoryData();
+                categoryData.amount = transaction.getAmount();
+                categoryData.count = 1;
+                categoryData.categoryName = transaction.getCategory().getName();
+
+                categoriesData.categoriesHashMap.put(transaction.getCategory().getId(), categoryData);
+            }
+
+            categoriesData.totalAmount += transaction.getAmount();
+            ++categoriesData.totalCount;
+        }
+
+        return categoriesData;
+    }
+
+    public static CategoriesData joinLowValueDataOverAmount(@NotNull CategoriesData categoriesData, double thresholdPercentage)
+    {
+        CategoriesData newCategoriesData = new CategoriesData();
+        newCategoriesData.categoriesHashMap = new HashMap<>();
+
+        final double thresholdAmount = categoriesData.totalAmount * (thresholdPercentage/100.0);
+        final double thresholdCount = categoriesData.totalCount * (thresholdPercentage/100.0);
+
+        Iterator it = categoriesData.categoriesHashMap.entrySet().iterator();
+
+        CategoryData others = new CategoryData();
+        others.categoryName = "Others";
+        while (it.hasNext())
+        {
+            final Map.Entry pair = (Map.Entry)it.next();
+            final CategoryData categoryData = (CategoryData) pair.getValue();
+
+            if(categoryData.amount <= thresholdAmount)
+            {
+                others.amount += categoryData.amount;
+                others.count += categoryData.count;
+            }
+            else
+            {
+                newCategoriesData.categoriesHashMap.put((Long) pair.getKey(), categoryData);
+            }
+        }
+
+        if(others.count != 0)
+        {
+            newCategoriesData.categoriesHashMap.put(-1L, others);
+        }
+
+        return newCategoriesData;
+    }
+}
---
+++ app/src/main/java/com/wdowiak/financemanager/dashboard/category/CategoriesData.java
@@ -0,0 +1,12 @@
+package com.wdowiak.financemanager.dashboard.category;
+
+import com.wdowiak.financemanager.dashboard.category.CategoryData;
+
+import java.util.HashMap;
+
+public class CategoriesData
+{
+    public HashMap<Long, CategoryData> categoriesHashMap;
+    public long totalCount;
+    public double totalAmount;
+}
